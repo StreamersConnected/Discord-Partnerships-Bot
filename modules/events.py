c14b5e4f454ce2c2e8268cf8f62f254ac51b6549
JustMaffie <jorivanee@hotmail.com>
2017-10-26 10:53:51 +0200

    Add some information on ready

--- modules/events.py
+++ modules/events.py
@@ -9,6 +9,11 @@ class Events:
 		game = discord.Game(name="with partners")
 		await bot.change_presence(status=discord.Status.idle, game=game)
 
+	async def on_ready(self):
+		info = ["\n", str(self.bot.user), f"Discord.py version: {discord.__version__}", f'Shards: {self.bot.shard_count}', f'Guilds: {len(self.bot.guilds)}',
+			f'Users: {len(set([m for m in self.bot.get_all_members()]))}', f'{len(self.bot.cogs)} modules with {len(self.bot.commands)} commands']
+		self.bot.logger.info("\n".join(info))
+
 def setup(bot):
 	cog = Events(bot)
 	bot.add_cog(cog)
